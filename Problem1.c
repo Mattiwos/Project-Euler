9baf915c3c657cbab784d83df215b6c88c92f693
Mattiwos Belachew <mattiwosbelachew@lich.local>
2020-03-02 21:25:07 -0800

    updated size

--- Problem1.c
+++ Problem1.c
@@ -1,4 +1,5 @@
-//Space: 4,248 bytes (8 K) after compiled  = 8 * 1024 =Kilobyte (KB)
+//Space used: Not compilled 547B
+    //Space: 4.15 KB after being compilied
 /*
 Problem 1:  Multiples of 3 and 5
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
