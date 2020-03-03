2c5544368e3f7ab1cc74766cf99a5018c5511748
Mattiwos Belachew <mattiwosbelachew@lich.local>
2020-03-02 19:18:22 -0800

    solved problem 1

---
+++ Problem1.c
@@ -0,0 +1,22 @@
+//Space: 4,248 bytes (8 K) after compiled  = 8 * 1024 =Kilobyte (KB)
+/*
+Problem 1:  Multiples of 3 and 5
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+# include <stdio.h>
+
+int main(){
+    int sum = 0;
+    int ad = 213243546;
+    int sd = 234567;
+
+    for (int i = 0; i < 1000; i++){
+        if (i %3 == 0 || i %5 == 0) sum+=i;
+    }
+   
+    return sum;
+    //output 233168
+    //923467th person
+}
